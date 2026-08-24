e372771875ded376421a5f39393203fa6d5e7eec
Brajesh Kamle <your-email@gmail.com>
2026-08-25 01:37:19 +0530

    Connect app to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.43.54:2100/api",
+  baseURL: "https://sargam-music-l2in.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
